6590ab91ae5cb1304434e10654fe44b87727d42f
nahmad94 <deevan11@gmail.com>
2022-12-06 21:44:21 -0500

    adds tests for mileage and description

--- test/supercar.test.js
+++ test/supercar.test.js
@@ -1,5 +1,39 @@
 const should = require('chai').should()
 const expect = require('chai').expect
 const supertest = require('supertest')
-const api = supertest(require('../app.js'))
+const api = supertest(require('../index'))
+
+describe('GET /model/:model/description', () => {
+    it('should return a 200 response', done => {
+        api
+            .get('/model/c63/description')
+            .set('Accept', 'application/json')
+            .end((err, res) => {
+                expect(response.body).to.include({
+                    model: "c63",
+                    make: "Mercedes",
+                    year: 2020,
+                    mileage: 42444
+                })
+            done()
+            })
+    })
+})
+
+describe('GET /mileage/:mileage', () => {
+    it('should return a 200 response', done => {
+        api
+            .get('/mileage/42444')
+            .set('Accept', 'application/json')
+            .end((err, res) => {
+                expect(response.body).to.include({
+                    model: "c63",
+                    make: "Mercedes",
+                    year: 2020,
+                    mileage: 42444
+                })
+            done()
+            })
+    })
+})
 
